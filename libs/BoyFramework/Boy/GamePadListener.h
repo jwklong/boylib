68aa931c9b31f2d2ed7e47bc743af22ca8845f47
jwklong <jwklong@gmx.com>
2026-07-09 18:15:43 +0100

    wip gamepad support
    Signed-off-by: jwklong <jwklong@gmx.com>

---
+++ libs/BoyFramework/Boy/GamePadListener.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "GamePad.h"
+#include "Mouse.h"
+
+namespace Boy
+{
+
+	class GamePadListener
+	{
+	public:
+
+		GamePadListener() {}
+		virtual ~GamePadListener() {}
+
+		virtual void gamePadButtonDown(GamePad *pad, GamePad::Button button) = 0;
+		virtual void gamePadButtonUp(GamePad *pad, GamePad::Button button) = 0;
+
+	};
+}
