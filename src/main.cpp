d0d98df513fa053943ae7d1f4a225ef6eb93865c
8 Bits <218309818+8-Bit-Bits@users.noreply.github.com>
2026-07-10 15:10:01 -0600

    Fix spacing in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char* args[]) {
 
 	Boy::Environment::instance()->init(
 		Demo2::instance(),
-        	800, // width
-        	600, // height
+        800, // width
+        600, // height
 		false, // fullscreen
 		"Asteroids (v1.0)", // window title
 		"demo1.dat"); // persistence layer file name
