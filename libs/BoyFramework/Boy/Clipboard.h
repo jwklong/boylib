25d9233291aac1f36183a11217d551dcc19ff164
jwklong <jwklong@gmx.com>
2026-07-17 13:16:38 +0100

    add Boy::Clipboard (also moved it from boylib)
    Signed-off-by: jwklong <jwklong@gmx.com>

---
+++ libs/BoyFramework/Boy/Clipboard.h
@@ -0,0 +1,7 @@
+#include <string>
+
+namespace Boy
+{
+	std::string getClipboardText();
+	void setClipboardText(const std::string &text);
+}
--- libs/BoyFramework/BoyLib/Clipboard.cpp
+++
@@ -1,9 +0,0 @@
-#include "Clipboard.h"
-
-//TODO: make this work, original implementation for BoyLib used a directX sdk and it only
-//      worked on windows (probably because it was only used for wog preview)
-
-std::string BoyLib::getClipboardText()
-{
-    return "";
-}
